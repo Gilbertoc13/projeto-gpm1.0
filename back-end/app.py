6b5431dd651aa61ef9219fb11c7aa4d22ad6ee30
Gilbertoc13 <workgpm2024@gmail.com>
2024-04-03 13:40:26 -0300

    Adicionando novas funcionalidades ao usuario

--- back-end/app.py
+++ back-end/app.py
@@ -15,7 +15,10 @@ DB_name = 'gpmDB'
 
 client = MongoClient(Mongo_URL)
 db = client[DB_name]
+#criação da coleção para usuarios(Login\Cadastro)
 usuarios_collection = db['usuarios']
+#criação da coleção filmes para usuario adicionar a sua lista
+filmes_collection = db['filmes']
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -46,6 +49,46 @@ def cadastro():
         return 'Usuário cadastrado com sucesso!'
 
     return render_template('cadastro.html')
+
+@app.route('/')
+def index():
+    filmes = filmes_collection.find()
+    return render_template('filmes.html',filmes = filmes)
+
+@app.route('/assistir/<filme_id>', methods=['POST'])
+def assistir_filme(filme_id):
+    if 'usuario_id' in session:
+        return redirect(url_for('index'))
+    else:
+        return redirect(url_for('login'))
+
+
+def get_filmes_vistos(usuario_id):
+    usuario = usuarios_collection.find_one({'_id': usuario_id})
+    if usuario:
+        filmes_vistos = usuario.get('filmes_vistos', []) 
+        return filmes_vistos
+    else:
+        return []
+    
+
+@app.route('/lista', methods=['GET'])
+def lista():
+    if 'usuario_id' in session:
+        usuario_id = session['usuario_id']
+        filmes_vistos = get_filmes_vistos(usuario_id)
+        if filmes_vistos:
+            filmes_info = []  
+            for filme_id in filmes_vistos:
+                filme = filmes_collection.find_one({'_id': filme_id})
+                if filme:
+                    filmes_info.append(filme)  
+            return render_template('lista.html', filmes=filmes_info)
+        else:
+            return 'Nenhum filme foi marcado como assistido ainda.'
+    else:
+        return redirect(url_for('login'))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
