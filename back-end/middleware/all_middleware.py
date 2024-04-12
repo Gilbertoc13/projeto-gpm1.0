0830684b08c6c707b846609ee0cc9ae61f7c1e45
Gilbertoc13 <workgpm2024@gmail.com>
2024-04-12 11:38:28 -0300

    backend

---
+++ back-end/middleware/all_middleware.py
@@ -0,0 +1,26 @@
+from flask import abort
+from models.Content import Movie
+from models.User import User
+
+
+def verify_user(userId):               
+    user = User.get_user_by_id_model(userId)
+    if not user:
+        abort(400, {"message": "User not exist"})
+    return user
+
+def verify_email_registered(email):          
+    user = User.get_user_by_email_model(email)
+    if user:
+        abort(400, {"message": "Email is not available"})
+    return {"message": "Email is available"}
+
+def verify_movie(movie_id):
+    verify_movie(movie_id)
+    movie = Movie.get_movie_by_id_model(movie_id)
+    previous_movie = movie.get("previousMovieId")
+    if previous_movie:
+        return previous_movie
+    return False
+
+
---
+++ back-end/routes/user_routes.py
@@ -0,0 +1,48 @@
+
+from flask import request,jsonify, Blueprint
+from flask_jwt_extended import jwt_required
+from controller.user_controller import login, create_user_controller, get_user_data
+from models.Content import Movie
+from models.User import User
+from flask import jsonify
+
+
+
+
+main_bp = Blueprint("main_bp", __name__)
+
+
+@main_bp.route("/api/login", methods=["POST"])
+def login_route():
+    data = request.get_json()
+    if "email" not in data or "password" not in data:
+        return jsonify({"message": "Faltando email ou senha!"}), 400
+
+    email = data["email"]
+    password = data["password"]
+
+    response, status_code = login(email, password)
+    return jsonify(response), status_code
+
+
+@main_bp.route("/api/cadastro", methods=["POST"])
+def create_user_route():
+    data = request.get_json()
+    if not all(key in data for key in ["username", "email", "password"]):
+        return jsonify({"message": "Missing required fields"}), 400
+
+
+    username = data["username"]
+    email = data["email"]
+    password = data["password"]
+
+    response, status_code = create_user_controller(email, username, password)
+    return jsonify(response), status_code
+
+
+@main_bp.route('/api/data_user', methods=['GET'])
+@jwt_required()
+def data_user_route():
+    return get_user_data()
+
+
