865b8b39e79bda779fb5e4c7c36476ac73515200
Gilbertoc13 <163465786+Gilbertoc13@users.noreply.github.com>
2024-05-02 14:58:08 -0300

    Feature/back end (#7)
    * backend
    
    * backend
    
    * backend
    
    * backend
    
    * erros no token
    
    * backend
    
    * ..
    
    * ..
    
    * deixando o username único
    
    * criando rota para obter nome do usuario e setando duração do token para 30 dias
    
    * adicionando rotas para pagina de detalhes.
    
    * eu tentei
    
    * backend
    
    * backend
    
    * backend
    
    * backend
    
    * Corrigindo identity do getUsername
    
    * rota comment adicionada
    
    * comentarios adicionados
    
    * comentarios adicionados
    
    * backend
    
    ---------
    
    Co-authored-by: Victor Morais <110771678+avictormorais@users.noreply.github.com>

---
+++ back-end/middleware/all_middleware.py
@@ -0,0 +1,27 @@
+from flask import abort
+from models.Media import MediaAPI
+from models.User import User
+from models.Comment import Comment
+
+
+def verify_user(userId):               
+    user = User.get_user_by_id_model(userId)
+    if not user:
+        abort(400, {"message": "User not exist"})
+    return user
+
+def verify_email_registered(email):          
+    user = User.get_user_by_email_model(email)
+    if user:
+        abort(400, {"message": "Email is not available"})
+    return
+
+def verify_username_registered(username):
+    user = User.get_user_by_username_model(username)
+    if user:
+        abort(401, {"message": "Username is not available"})
+    return
+
+
+
+
